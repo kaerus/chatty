e6797362c4cca71c7d06646728056afc75524998
anders elo <anders@kaerus.org>
2012-06-27 01:00:52 +0200

    Initial commit

---
+++ chatty-server.js
@@ -0,0 +1,215 @@
+/* 
+ * Chatty chat server with irc like commands
+ *
+ * Developed by anders @ kaerus com 2012.
+ * MIT license, feel free to use, share and modify. 
+ */
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , url = require('url')
+  , util = require('util')
+  , path = require('path')
+  , extend = require('node.extend')
+  , arango = require('arango')
+  , template = require('./lib/template')
+  , config = {
+    _config: {  "app": "chatty",
+                "name":	"Chatty server",
+                "version": "0.1.2"
+    },
+    show_help: function(){
+      console.log("Command line options:","arguments --server <hostname>" +
+	                                        " --port <server port>" +
+	                                        " --template <html template>");
+	    process.exit(0);
+    },
+    get: function(){
+	    /* parse command line options */
+      for( var i = 0, option = process.argv; option[i]; i++) {
+        switch(option[i]) {
+          case '--help':
+            this.show_help();
+            break;
+          case '--server':
+            this._config.server = option[++i];
+            break;
+          case '--port':
+            this._config.listen = option[++i];
+            break;
+          case '--template':
+            this._config.template = option[++i];
+            break;
+          case '--config':
+            this._config.file = option[++i];
+            break;
+        }
+      }
+       try { 
+		    extend(this._config,JSON.parse(fs.readFileSync(this._config.file, 'utf8'))); 
+	    }	catch(err) { 
+		    if(process.argv.length < 3){
+	 		    this.show_help();
+		    } 
+	    }
+	    return this._config;
+    }
+}.get();
+
+process.on('exit', function(code,signal){
+	console.log("Process uptime %s, exit",process.uptime(), code ? code : "", signal ? signal : "" );
+});
+
+console.log("%s configuration:\n", config.name,util.inspect(config) );
+
+var layout = template.loadTemplate(config.template);
+
+var db = new arango.Connection(config.db).on('error',function(err){
+	console.log("DB Error:",util.inspect(err));
+});
+
+console.log("Waiting for requests on port", config.listen);
+
+app.listen(config.listen);
+function handler (req, res) {
+  var u = url.parse(req.url,true);
+  var filePath = './public' + u.pathname;
+         
+  var extname = path.extname(filePath) || "";
+  var contentType = 'text/html';
+  switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.aiff':
+            contentType = 'audio/mpeg';
+            break;
+        case '.mp3':
+            contentType = 'audio/mp3';
+            break;
+  }
+  if(filePath === './public/') {
+    // Respond with html template
+	  res.writeHead(200);
+	  res.end(layout({server:config.server+':'+config.listen}));
+	} else {
+	  // Respond with file
+    path.exists(filePath, function(exists) {
+      if (exists) {
+        fs.readFile(filePath, function(error, content) {
+          if (error) {
+            // File read error
+            res.writeHead(500);
+            res.end();
+          }
+          else {
+            // Send file content to client
+	      res.writeHead(200,{"Content-Type":contentType });
+	      res.end(content);
+	  }        
+	}); 
+      } else {
+     	 // File not found
+   	res.writeHead(404);
+    	res.end("not found");
+      }
+    });
+  }
+}
+
+var names = {};
+
+function storeMessage(timestamp,name,msg,type){
+  var path = "/_api/document?collection=chat&createCollection=create";
+  var data = {ch: "",timestamp:timestamp,name:name,msg:msg,type:type};
+	db.request.post(path,data,function(res){
+  }).on('error',function(err){	
+      console.log("Store error:", util.inspect(err) );
+  });
+        						
+}
+
+function broadcast(msg,timestamp,name,data,type){
+    this.broadcast.emit(msg,timestamp,name,data,type);
+    storeMessage(timestamp,name,data,type);
+}
+
+io.sockets.on('connection', function (socket) {
+     	
+  var server_msg = config.name +' v '+ config.version;               
+  socket.emit('server', server_msg);
+ 
+ 
+  socket.on('disconnect', function() {
+    var name = socket.store.data.nickname;
+    var timestamp = new Date().getTime(); 
+    delete names[name];
+    socket.broadcast.emit('user disconnected',timestamp,name);
+  });
+  
+  socket.on('set nickname', function (n) {
+    /* escape the name */
+    var timestamp = new Date().getTime()
+      , name = escape(n); 
+    if(names[name]) {
+      if(names[name]!==socket.id)
+        socket.emit('nick taken',name);
+    } else {
+      socket.get('nickname',function(err,oldname){
+	if (oldname) socket.broadcast.emit("user changed nick", timestamp, oldname, name);
+	else socket.broadcast.emit("user joined", timestamp, name);	
+        socket.emit('nick ready',timestamp,name);
+        socket.set('nickname',name);
+        if(oldname) delete names[oldname];
+        names[name] = socket.id;
+      });
+   }   
+  });
+
+  socket.on('msg', function (timestamp,m) {
+    /* escape the message */
+    var message = escape(m);
+    socket.get('nickname', function (err, name) {
+      var now = new Date().getTime();
+      if( now - 10000 < timestamp ) {
+        broadcast.call(socket,"msg", timestamp, name, message, "public");
+	      socket.emit("received",timestamp);
+      } else {   
+        socket.emit("discarded",timestamp);
+	    }
+    });
+  });
+  
+  /* return list of names */
+  socket.on('get users', function(){
+    socket.emit('users',Object.keys(names));
+  });
+ 
+  /* return (user) history */
+  socket.on('get history', function(name,count,offset){
+    var filter = name ? "FILTER u.name == '" + escape(name) +"'" : "";
+	  db.request.post("/_api/cursor",
+			{query: "FOR u IN @@collection " + filter + 
+				" LIMIT " + parseInt(offset) + " , " + parseInt(count) +
+				" SORT u.timestamp DESC RETURN [u.timestamp,u.name,u.msg,u.type]", 
+			 bindVars: {"@collection": "chat"}}, function(ret) {
+	 	if(ret.result.length > 0)
+	    		socket.emit("history", ret.result);
+	  }).on('error',function(err){
+	    util.log("history error:", util.inspect(err) );
+	  });	 
+  });
+});
---
+++ public/js/chatty-client.js
@@ -0,0 +1,153 @@
+jQuery(document).ready(function($){
+  var server = $("#server").text();
+  
+  console.log("Connecting to server:", server);
+  var socket = io.connect("192.168.0.100:8088");
+  
+  var _storage = (function() {
+    if( typeof localStorage === 'object' ) 
+      return localStorage;
+    else if( typeof globalStorage === 'object' )
+      return globalStorage[location.host];
+    else return "";  
+  })();
+  
+  var nick = {
+    _name: null,
+    get name(){
+      if(_storage.nickname){
+        this._name = _storage.nickname; 
+      } else this._name = "Anon" + Math.floor(Math.random()*1000+1000);
+      return this._name;
+    },
+    set name(val){
+      this._name = val;
+      _storage.nickname = val;
+    }
+  };  
+  
+  function appendMessage(timestamp, name, message, type, prepend){
+    var t = timestamp ? new Date(timestamp) : new Date();
+    var time = t.toTimeString().split(' ')[0];
+    var datetime = t.toISOString();
+    var html = '<article id="'+timestamp+'" class="chat '+type+'">' +
+               '<header><time pubdate datetime="'+datetime+'">'+time+'</time></header>' +
+               (name ? '<b>'+unescape(name)+'</b>' : '')+
+               '<mark>'+unescape(message)+ '</mark>' +
+               '</article>';
+    
+      if(!prepend) $('section#content','section#chat').append(html);
+      else $('section#content','section#chat').prepend(html);
+      $("#sound").attr('src',"sounds/"+$("#select").val());
+    var el = document.getElementById(timestamp);  
+    el.scrollIntoView(true);      
+  }
+  
+  socket.on('msg', function() {
+    appendMessage.apply(this,arguments);
+  });
+  
+  socket.on('connect',function(){
+    if(nick.name === null) {
+      socket.emit('get history',null,100,0);
+    }
+	  this.emit('set nickname',unescape(nick.name));
+	  socket.emit('get users');
+  });
+  
+  socket.on('history',function(messages) {
+    for(var i = 0; i < messages.length; i++){
+      messages[i].push('prepend');
+      appendMessage.apply(this,messages[i]);
+    }
+  });
+
+  function addUser(user){
+    var html = '<article id="' + user + '" class="user">' +
+              '<b>'+unescape(user)+'</b>' +
+              '</article>';
+    $('section#users').append(html);         
+  }
+  
+  socket.on('users',function(users){
+    $('section#users article.user').remove();
+    for(var i = 0; i < users.length; i++) {
+      addUser(users[i]);
+    }
+  });
+  
+  socket.on('user changed nick',function(timestamp,old,name){
+     $('section#users article#'+old).remove();
+     addUser(name);
+  });
+  
+  socket.on('user disconnected', function(timestamp,name){
+    $('section#users article#'+name).remove();
+  });
+  
+  socket.on('user joined',function(timestamp,name){
+    addUser(name);
+  });
+  
+  socket.on('server',function(msg){
+    appendMessage(null,null,msg,"server");
+  });
+  
+  socket.on('nick ready',function(timestamp,name){
+	  appendMessage(timestamp,nick.name,"Your nickname is now " + unescape(name),"server");
+	  $('section#users article#'+nick.name).remove();
+	  addUser(name);
+	  nick.name = name;
+	});
+	
+	socket.on('nick taken',function(name){
+	  var newname = name + Math.floor(Math.random()*1000+1000);
+	  socket.emit('set nickname',newname);
+	});
+	
+	socket.on('received',function(timestamp){
+	  $("article.chat#"+timestamp).removeClass("pending");
+	});
+	
+	socket.on('discarded',function(timestamp,message){
+	  $("article.chat#"+timestamp).addClass("discarded").removeClass("pending"); 
+	});
+	
+  $("#send-message").bind('change',function() { 
+	  var msg = this.value;
+	  if(msg[0] === '/') {
+		  var v = 0
+		    , arg = msg.split(' ')
+		    , cmd = arg[v++].slice(1);
+		  switch( cmd ) {
+		    case 'nick':
+			    socket.emit("set nickname",arg[v]);
+			    break;
+		    case 'history': 
+		      var user = null;
+		      if( !(Number(arg[v]) >= 0) ) { user = arg[v++]; }
+		      var count = arg[v++] || 100, offset = arg[v++] || 0;
+		      socket.emit('get history',user,count,offset);
+		      break;
+		    case 'clear':
+		      var timestamp = new Date().getTime();
+		      var t = arg[v] || 0; 
+		      $("article.chat").each(function(element){
+	          if( this.id < timestamp - t ) $(this).remove();
+	        });
+	        break;
+	       case 'users':
+	        socket.emit("get users");
+	        break;
+	      default: break;  
+		  }
+	  } else {
+	    var timestamp = new Date().getTime();
+		  socket.emit("msg",timestamp,msg);
+		  appendMessage(timestamp,nick.name,msg,"self pending");
+	  }
+	    this.value = "";
+	}); 
+	
+	
+});
