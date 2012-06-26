34271825bf81778e1f31080651ce475b55ed2491
anders elo <anders@kaerus.org>
2012-06-27 01:16:13 +0200

    Added license stuff

--- chatty-server.js
+++ chatty-server.js
@@ -1,8 +1,26 @@
 /* 
  * Chatty chat server with irc like commands
  *
- * Developed by anders @ kaerus com 2012.
- * MIT license, feel free to use, share and modify. 
+ * Developed by anders @ kaerus com.
+ * MIT licensed, feel free to use, share and modify. 
+ *
+ * Copyright (c) 2012 <anders@kaerus.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  */
 
 var app = require('http').createServer(handler)
